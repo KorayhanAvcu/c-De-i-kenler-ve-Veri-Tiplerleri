73eaad6cb12a9eed8025cd86017979542f06ff1b
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-10-25 20:36:52 +0300

    Add files via upload

---
+++ Değişkenler/deneme/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* 
+int deger;
+deger=2;
+Console.WriteLine(deger);
+ */
+
+ /* 
+
+ string degisken=null;
+ string Degisken=null;
+  */
+  /* 
+  Bu olmaz 
+  string degisken+veri=null;
+   */
+ /* 
+  Bu olmaz 
+  string degisken veri=null;
+*/
+/* 
+    string degisken =" ";
+ */
+
+/* 
+byte b=5 //1byte
+sbyte c=2 //1byte
+
+short s = 5 //2 byte
+ushort us = 5 //2 byte
+
+Int16 i16 = 5 //2 byte
+int i=2 // 4byte
+Int32 i32=2 //4byte
+Int64 i64=2 //8byte
+
+uint ui=2 //4byte
+long l=4; //8byte
+ulong ul=4; //8byte
+
+
+//reel sayilar
+float f=5; //4byte
+double d=5; //8byte
+decimal de=5; //16 byte
+
+char ch='2'; // 2byte 
+string str="korayhan" //sınırsız
+bool b1 = true;
+bool b2 = false;
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+bool b1 = true;
+bool b2 = false;
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+
+
+// boolean
+bool bool1 = 10>2;
+// Değişken Dönüşümleri
+string str20 = "20";
+int int20 = 20;
+string yeniDeger = str20+ int20.ToString
+Console.WriteLine(yeniDeger);
+
+int int21 = int20+ Convert.ToInt32 (str20);
+Console.WriteLine(int21); // Çıktısı 40
+
+int int22= int20+ int.Parse(str20); // Çıktısı 40 
+
+// datetime
+string datetime = DateTime.Now. ToString("dd.MM.yyyy");
+Console.WriteLine(datetime);
+string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+Console.WriteLine(datetime2);
+string hour = DateTime.Now. ToString("HH:mm");
+Console.WriteLine(hour);
+*/
